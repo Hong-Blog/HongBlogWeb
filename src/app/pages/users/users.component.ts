ffa33e4dbaf2920e6ab61a809853487fe36fc7e6
hong <lun3322@126.com>
2020-11-04 00:13:36 +0800

    列表例子

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+interface Person {
+  key: string;
+  name: string;
+  age: number;
+  address: string;
+}
 
 @Component({
   selector: 'app-users',
@@ -6,8 +13,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.less']
 })
 export class UsersComponent implements OnInit {
+  listOfData: Person[] = [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park'
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park'
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park'
+    }
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
--- src/app/pages/users/users.module.ts
+++ src/app/pages/users/users.module.ts
@@ -1,10 +1,17 @@
 import {NgModule} from '@angular/core';
 import {UsersComponent} from './users.component';
 import {UsersRoutingModule} from './users-routing.module';
-
+import {NzTableModule} from 'ng-zorro-antd/table';
+import {CommonModule} from '@angular/common';
+import {NzDividerModule} from 'ng-zorro-antd/divider';
 
 @NgModule({
-  imports: [UsersRoutingModule],
+  imports: [
+    UsersRoutingModule,
+    NzTableModule,
+    CommonModule,
+    NzDividerModule
+  ],
   declarations: [UsersComponent],
   exports: [UsersComponent]
 })
